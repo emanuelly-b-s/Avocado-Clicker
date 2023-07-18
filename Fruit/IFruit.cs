057698d6e14621cadcdc2e889e259e23219d2c93
Emanuelly B. Silva <emanuellybs14@gmail.com>
2023-07-18 16:44:18 -0300

    interface fruit created

---
+++ Fruit/IFruit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit;
+
+internal interface IFruit
+{
+    public string Name { get; set; }
+    public float ActualGenerateSegunds { get; set; }
+}
+
